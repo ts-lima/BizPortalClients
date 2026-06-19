df5c9256301d32d54cc8e7814ecc14a69c4b436a
リマ <168151544+ts-lima@users.noreply.github.com>
2026-06-19 16:12:52 +0900

    Djangoにユーザーパスワード更新機能を改善し、有効状態の変更を追加。ログインのUXを改善し、バージョン1.3.0に更新。

--- django_bizportal_client/settings.py
+++ django_bizportal_client/settings.py
@@ -8,9 +8,10 @@ DEFAULTS = {
     'OIDC_STATE_MAX_AGE_SECONDS': 300,
     'OIDC_TIMEOUT_SECONDS': 10,
     'OIDC_IDENTITY_MODEL': 'django_bizportal_client.OIDCIdentity',
-    'OIDC_AUTH_BACKEND': 'django_bizportal_client.backends.BizPortalOIDCBackend',
+    'OIDC_AUTH_BACKEND': 'django.contrib.auth.backends.ModelBackend',
     'OIDC_AUTO_LINK_BY_EMAIL': False,
     'OIDC_AUTO_CREATE_USER': False,
+    'OIDC_LOGIN_REQUIRED_USER_ATTRS': {'is_active': True},
 }
 
 
@@ -34,7 +35,7 @@ def get_required_setting(setting_name):
 
 
 def get_oidc_identity_model():
-    model_path = get_setting('OIDC_IDENTITY_MODEL', DEFAULTS['OIDC_IDENTITY_MODEL'])
+    model_path = get_setting('OIDC_IDENTITY_MODEL')
     if not model_path or '.' not in model_path:
         raise ImproperlyConfigured('OIDC_IDENTITY_MODEL must be in the format "app_label.ModelName".')
     app_label, model_name = model_path.split('.', 1)
